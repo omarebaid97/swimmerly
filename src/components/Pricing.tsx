205345770a253af169c45959fe3a240fa37a77af
omarebaid97 <156451357+omarebaid97@users.noreply.github.com>
2024-03-06 11:16:53 -0600

    Update Pricing.tsx

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -4,6 +4,7 @@ export default function Pricing() {
     return (
         <div className="overflow-x-auto mt-10">
             <h2 className="text-3xl font-bold text-center mb-5">Pricing</h2>
+            <h3 className="text-xl font-bold text-center mb-5">Payment Is Due Every 4 Classes</h3>
             <div className="flex flex-col md:flex-row text-left">
                 <div className="card w-80 bg-neutral text-neutral-content m-5">
                     <div className="card-body">
